740212eb2c329a207de7e8107d4fded7149e4483
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-01-24 15:10:58 +0100

    fix: fix 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,41 +1,84 @@
-const { stdin, stdout, exit } = process;
 const fs = require("fs");
 const path = require("path");
 
-(function copyDir() {
-  fs.mkdir(
-    path.join(__dirname, "files-copy"),
-    (err) => {
-        if (err) throw err;
-    }
-  )
-
-reader = fs.createReadStream(
-    path.join(__dirname, "files"),
-)
-reader.on("data", (err, data) => {
-    if(err) throw err
-    stdout.write(data.toString())
-})
-
 fs.readdir(
     path.join(__dirname, "files"),
-    {withFileTypes: true},
-    (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-        if (file.isFile()){
-            fs.copyFile(
-                path.join(__dirname, "files", file.name),
-                path.join(__dirname, "files-copy", file.name),
-                (err) => {
-                    if (err) throw err;
+    (err) => {
+        if (err) throw err;
+        fs.stat(
+            path.join(__dirname, "files"),
+            (err, stats) => {
+                if (err) throw err;
+                if (stats.isDirectory()) {
+                    fs.stat(
+                        path.join(__dirname, "files-copy"),
+                        function(err) {
+                        if (!err) {
+                            console.log('Директория есть');
+                            fs.rm(
+                                path.join(__dirname, "files-copy"),
+                                {recursive: true, force: true},
+                                (err) => {
+                                    if (err) throw err;
+                                    fs.mkdir(
+                                        path.join(__dirname, "files-copy"),
+                                        {recursive: true, force: true},
+                                        (err) => {
+                                            if (err) throw err;
+                                        }
+                                    )
+
+                                    fs.readdir(
+                                        path.join(__dirname, "files"),
+                                        { withFileTypes: true },
+                                        (err, files) => {
+                                            if (err) throw err;                                    
+                                            files.forEach((file) => {
+                                                fs.copyFile(
+                                                    path.join(__dirname, "files", file.name),
+                                                    path.join(__dirname, "files-copy", file.name),
+                                                    (err) => {
+                                                        if (err) throw err;
+                                                    }
+                                                );
+                                            })
+                                        }
+                                    )  
+                                }
+                            )
+                        } else if (err.code === 'ENOENT') {
+                            console.log('директории нет');
+                            fs.mkdir(
+                                path.join(__dirname, "files-copy"),
+                                {recursive: true, force: true},
+                                (err) => {
+                                    if (err) throw err;
+                                }
+                            )
+                        }
+                    });
                    
+                    
+                    fs.readdir(
+                        path.join(__dirname, "files"),
+                        { withFileTypes: true },
+                        (err, files) => {
+                            if (err) throw err;
+                            files.forEach((file) => {
+                                console.log(file.name)
+                                fs.copyFile(
+                                    path.join(__dirname, "files", file.name),
+                                    path.join(__dirname, "files-copy", file.name),
+                                    (err) => {
+                                        if (err) throw err;
+                                    }
+                                );
+                            })
+                        }
+                    )
                 }
-            )
-        }
-    });
-})
-
-}())
-
+            }
+            
+        )
+    }
+)
